3e73734ea2825b45ade3d3e9f8a51df2279922df
ubaidGanai <ubaidshabir837@gmail.com>
2026-04-10 21:00:51 +0530

    Use environment variable for server port

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/api/status/:id', (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running! Open http://localhost:${PORT} in your browser.`);
 });
